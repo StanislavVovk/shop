7c5eb3e7417d8f8337be6120db5fcefe53ead2aa
Stas Siuris <stassiuris@gmail.com>
2023-05-27 13:04:55 +0300

    fix shop item display

--- client/src/components/Pages/Shop/components/ShopsItems.tsx
+++ client/src/components/Pages/Shop/components/ShopsItems.tsx
@@ -7,17 +7,16 @@ import { useAppSelector } from 'common/common';
 export const ShopsItems: FC = () => {
   const shops = useAppSelector(state => state.shopReducer.shops)
   const shopName = useAppSelector(state => state.shopItemSwitcherReducer.shopName)
-
   const data = shops?.filter(item => item.name === shopName)
-  const mappedData = data?.map((item, index) => item.items[index])
+  const mappedData = data?.map((item) => item.items)
   return (
     <Col md={12} xs={12} lg={{ span: 7, offset: 1 }} className={`${style.ColWrapper}`}>
       <Toast className={`w-100 h-100 p-0 ${style.ShopItems}`}>
         <Toast.Body>
           <Row xs={1} md={2} lg={2} className={`justify-content-evenly g-4 px-5 ${style.ShopItemsRow}`}>
-             {mappedData?.map((item, key) => {
-               return item && <ShopCard key={key} item={item}/>
-             })}
+              {mappedData?.map((item) => {
+                return item.map((data, key) => <ShopCard key={key} item={data}/>)
+              })}
           </Row>
         </Toast.Body>
       </Toast>
